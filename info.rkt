ec388b0d548416d2b8848ee22286c805ba0a5be6
Jay McCarthy <jay.mccarthy@gmail.com>
2015-10-02 14:30:03 -0400

    deps

--- info.rkt
+++ info.rkt
@@ -1,5 +1,7 @@
 #lang info
 (define version "0.0")
 (define collection 'multi)
-(define deps '())
-(define build-deps '("base" "racket-doc" "scribble-lib"))
+(define deps '("base"
+               ))
+(define build-deps '("rackunit-lib"
+                     "base" "racket-doc" "scribble-lib"))
