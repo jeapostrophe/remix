46504f94d8512c9fe85d1c236c7e0cf225b8c490
Jay McCarthy <jay.mccarthy@gmail.com>
2015-10-03 18:04:22 -0400

    examples

--- remix/exp/reader.rkt
+++ remix/exp/reader.rkt
@@ -33,18 +33,21 @@
   (check-false (read-cdot))
   
   (testit*
+   ["(1 2 3)" (1 2 3)]
    ["[1 2 3]" (#%brackets 1 2 3)]
    ["{1 2 3}" (#%braces 1 2 3)]
+   ["|a.b|" a.b]
    ["a.b" (#%dot a b)]
    ["a .b" (#%dot a b)]
    ["a. b" (#%dot a b)]
    ["a . b" (#%dot a b)]
    ["1.a" (#%dot 1 a)]
-   ["1.2.a" (#%dot 1.2 a)]
+   ["#i1.2 .a" (#%dot 1.2 a)]
    ["1 .2.a" (#%dot 1 (#%dot 2 a))]
-   ["a.1.2" (#%dot a 1.2)]
+   ["a.#i1.2" (#%dot a 1.2)]
    ["a.b.c" (#%dot a (#%dot b c))]
    ["a.(b c)" (#%dot a (b c))]
    ["(a b).c" (#%dot (a b) c)]
    ["(a b).(c d)" (#%dot (a b) (c d))]
-   ["(a b).[3]" (#%dot (a b) (#%brackets 3))]))
+   ["(a b).[3]" (#%dot (a b) (#%brackets 3))]
+   ["({1})" ((#%braces 1))]))
