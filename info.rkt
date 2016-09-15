b293c15b1893ef342ae777dd99d10fe9c27c7df4
Georges Dupéron <georges.duperon@gmail.com>
2016-09-16 00:01:46 +0200

    Fix missing dependencies in info.rkt

--- info.rkt
+++ info.rkt
@@ -2,6 +2,7 @@
 (define version "0.0")
 (define collection 'multi)
 (define deps '("base"
-               ))
+               "at-exp-lib"
+               "rackunit-lib"))
 (define build-deps '("rackunit-lib"
                      "base" "racket-doc" "scribble-lib"))
