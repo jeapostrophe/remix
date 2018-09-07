3bc3d825d99d3f202751b0d78c4d7940cdad119a
Jay McCarthy <jay.mccarthy@gmail.com>
2018-09-07 16:25:23 -0400

    Remove core

--- remix/main.rkt
+++ remix/main.rkt
@@ -1,7 +1,7 @@
 #lang racket/base
 
 (module reader syntax/module-reader
-  remix/core
+  remix/main
   #:read at:read
   #:read-syntax at:read-syntax
   #:wrapper1
@@ -30,3 +30,49 @@
        (try-dynamic-require 'scribble/private/indentation 'keystrokes)]
       [else (fallback)]))
   (require (prefix-in at: scribble/reader)))
+
+(require (for-syntax racket/base
+                     syntax/parse)
+         remix/required-helper
+         racket/default-in)
+
+;; xxx remove need for local-require/local-require* by detecting context?
+
+(define-syntax (remix-module-begin stx)
+  (syntax-parse stx
+    #:literals (remix-require*)
+    [(_ (~and s (~not (remix-require* . _))) ...)
+     (syntax/loc stx
+       (#%module-begin s ...))]
+    [(_ (~and pre (~not (remix-require* . _))) ...
+        (remix-require* m)
+        post ...)
+     (syntax/loc stx
+       (#%module-begin pre ... (do-remix-require* m post ...)))]))
+
+(define-syntax (do-remix-require* stx)
+  (syntax-parse stx
+    [(_ m . body)
+     (syntax/loc stx
+       (begin (require (rename-in m [#%require*d internal-#%require*d]))
+              (internal-#%require*d . body)))]))
+
+(define-syntax (remix-require stx)
+  (syntax-parse stx
+    [(_ m)
+     (syntax/loc stx
+       (begin (require (rename-in (default-in m
+                                    [#%required default-#%required])
+                                  [#%required internal-#%required]))
+              (internal-#%required m)))]
+    [(_ m ...)
+     (syntax/loc stx
+       (begin (remix-require m) ...))]))
+
+(define-syntax (remix-require* stx)
+  (raise-syntax-error 'require* "illegal outside of top-level" stx))
+
+(provide (rename-out
+          [remix-module-begin #%module-begin]
+          [remix-require require]
+          [remix-require* require*]))
